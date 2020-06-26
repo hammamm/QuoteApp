f7de45656ab6a2cd2d3346d8637c613928029943
hammam <hammamo2010@gmail.com>
2020-06-26 16:13:15 +0300

    Initial Commit

---
+++ QuoteApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuoteApp
+//
+//  Created by hammam abdulaziz on 05/11/1441 AH.
+//  Copyright © 1441 randomQuote. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
